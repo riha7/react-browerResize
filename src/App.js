b3c57da03d1e71c48eb6f80acb9869f56004dd52
riha7 <harik0725@gmail.com>
2021-08-30 08:18:31 +0530

    useEffect Hook Added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React,{useState,useEffect} from "react";
-import "./style.css";
-const useWindowResize=()=>{
-  const [size,setsize] = useState([window.innerHeight,window.innerWidth])
-  useEffect(()=>{
-    const handleResize=()=>{
-      setsize([window.innerHeight,window.innerWidth])
-    }
-    window.addEventListener("resize",handleResize)
-  })
+import React, { useState, useEffect } from 'react';
+import './style.css';
+const useWindowResize = () => {
+  const [size, setsize] = useState([window.innerHeight, window.innerWidth]);
+  useEffect(() => {
+    const handleResize = () => {
+      setsize([window.innerHeight, window.innerWidth]);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('remove', handleResize);
+    };
+  }, []);
   return size;
-}
+};
 export default function App() {
-  const [innerHeight,innerWidth] = useWindowResize()
+  const [innerHeight, innerWidth] = useWindowResize();
   return (
     <div>
       <h1>height : {innerHeight}</h1>
